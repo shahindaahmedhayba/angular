280c3e317cf2bc83285c4e6dfd8b11e867588540
shahindaahmedhayba <shahindahayba3@gmail.com>
2025-03-23 16:51:18 +0200

    Add files via upload

---
+++ portfilio/js/script.js
@@ -0,0 +1,16 @@
+
+console.log("Welcome to Shahinda's Portfolio!");
+
+
+document.querySelector(".btn-dark").addEventListener("click", function() {
+  alert("Opening GitHub Profile...");
+});
+
+
+document.querySelector(".btn-info").addEventListener("click", function() {
+  alert("Opening LinkedIn Profile...");
+});
+
+
+
+
